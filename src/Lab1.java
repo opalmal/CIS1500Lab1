b1e92cef3700158e31c25734cc4a971621907a50
opalmal <seitz.olivia@student.oaklandcc.edu>
2022-05-15 20:53:00 -0400

    Lab1 finished check in

---
+++ src/Lab1.java
@@ -0,0 +1,11 @@
+public class Lab1 {
+
+    public static void main(String[] args) {
+        System.out.println("Russel Day is taking this class as a guest student to learn more about computer science.");
+        System.out.println("He has little experience, but knows some Python.");
+        System.out.println("Russel's hobbies are reading, video games, and running.");
+        System.out.println("This summer he is going to Thailand and Dominican Republic for service trips.");
+        System.out.println("Russel looks forward to learning some Java wih you all!");
+
+    }
+}
